35a8c7e375aefd5c34f12e9085e91b0c52311c46
Chris Calo <ccalo@vulcanca.com>
2019-03-12 01:20:48 -0400

    Ensured preact hydration

--- src/browser/index.js
+++ src/browser/index.js
@@ -3,8 +3,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.render((
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-), document.getElementById('root'));
+ReactDOM.render(
+  (
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  ),
+  document.getElementById('root'),
+  document.getElementById('root')
+);
