55ffc9d657197a63ded64ebf88cc8ba6d6acd1a2
Chris Calo <ccalo@vulcanca.com>
2019-03-15 21:36:52 -0400

    Deferred non-critical styling as long as possible

--- config/webpack-production.js
+++ config/webpack-production.js
@@ -229,7 +229,7 @@ const browserConfig = {
       filename: 'static/styles/main.css',
     }),
     new Critters({
-      // preload: 'swap',
+      preload: 'js-lazy',
       logLevel: 'silent',
       pruneSource: false,
       preloadFonts: true,
