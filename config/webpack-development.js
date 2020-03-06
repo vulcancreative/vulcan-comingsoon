d4bc7feb6263b8be5f6cb052861bcc5abffb68ad
Chris Calo <ccalo@vulcanca.com>
2020-03-06 14:27:06 -0500

    Replaced Webpack configs with updated versions

--- config/webpack-development.js
+++ config/webpack-development.js
@@ -1,13 +1,13 @@
-const os = require('os');
-const path = require('path');
-const ip = require('my-local-ip');
-const colors = require('colors');
-const process = require('process');
-const webpack = require('webpack');
-const childProcess = require('child_process');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const eslintFormatter = require('react-dev-utils/eslintFormatter');
-const MiniCssExtractPlugin = require('extract-css-chunks-webpack-plugin');
+const os = require("os");
+const path = require("path");
+const ip = require("my-local-ip");
+const colors = require("colors");
+const process = require("process");
+const webpack = require("webpack");
+const childProcess = require("child_process");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const eslintFormatter = require("react-dev-utils/eslintFormatter");
+const MiniCssExtractPlugin = require("extract-css-chunks-webpack-plugin");
 
 const envPort = process.env.PORT;
 const port = envPort ? envPort : 4000;
@@ -16,130 +16,125 @@ const isPrivate = process.env.PRIVATE || false;
 
 const platform = os.platform().toLowerCase();
 
-const isLinux = platform === 'linux';
-const isWindows = platform === 'win32' || platform === 'win64';
+const isLinux = platform === "linux";
+const isWindows = platform === "win32" || platform === "win64";
 
-if (!process.env.NODE_ENV) process.env.NODE_ENV = 'development';
+if (!process.env.NODE_ENV) process.env.NODE_ENV = "development";
 const mode = process.env.NODE_ENV;
 
 const linter = {
   test: /\.(js|jsx|mjs)$/,
-  enforce: 'pre',
+  enforce: "pre",
   use: [
     {
       options: {
         formatter: eslintFormatter,
-        eslintPath: require.resolve('eslint'),
-        
+        eslintPath: require.resolve("eslint")
       },
-      loader: require.resolve('eslint-loader'),
-    },
+      loader: require.resolve("eslint-loader")
+    }
   ],
-  include: path.resolve(__dirname, 'src'),
+  include: path.resolve(__dirname, "src")
 };
 
 const imgLoad = {
   test: /\.(gif|png|jpe?g)$/i,
-  loader: require.resolve('url-loader'),
+  loader: require.resolve("url-loader"),
   options: {
     limit: 10000,
-    name: 'static/media/[name].[hash:8].[ext]',
-  },
+    name: "static/media/[name].[hash:8].[ext]"
+  }
 };
 
 const jsLoad = {
   test: /\.(js|jsx|mjs)$/,
-  loader: require.resolve('babel-loader'),
+  loader: require.resolve("babel-loader"),
   options: {
-    cacheDirectory: true,
+    cacheDirectory: true
   },
-  exclude: /(node_modules)/,
+  exclude: /(node_modules)/
 };
 
 const fontLoad = {
   test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
   use: [
     {
-      loader: 'file-loader',
+      loader: "file-loader",
       options: {
-        name: 'static/fonts/[name].[hash:8].[ext]',
-      },
-    },
-  ],
+        name: "static/fonts/[name].[hash:8].[ext]"
+      }
+    }
+  ]
 };
 
 const sassLoad = {
   test: /\.s?(a|c)ss$/,
   use: [
-    mode.includes('dev') ? 'style-loader' : MiniCssExtractPlugin.loader,
+    mode.includes("dev") ? "style-loader" : MiniCssExtractPlugin.loader,
     {
-      loader: 'css-loader',
+      loader: "css-loader"
     },
     {
-      loader: 'sass-loader',
-    },
-  ],
+      loader: "sass-loader"
+    }
+  ]
 };
 
 const fileLoad = {
   exclude: [/\.(js|jsx|mjs)$/, /\.html$/, /\.json$/],
-  loader: require.resolve('file-loader'),
+  loader: require.resolve("file-loader"),
   options: {
-    name: 'static/media/[name].[hash:8].[ext]',
-  },
+    name: "static/media/[name].[hash:8].[ext]"
+  }
 };
 
 module.exports = {
   mode: mode,
-  stats: 'errors-only',
-  entry: './src/browser/index.js',
+  stats: "errors-only",
+  entry: "./src/browser/index.js",
   output: {
-    path: path.resolve(__dirname, '..', 'src', 'public'),
-    filename: 'static/scripts/bundle.js',
-    publicPath: '/',
+    path: path.resolve(__dirname, "..", "src", "public"),
+    filename: "static/scripts/bundle.js",
+    publicPath: "/"
   },
   module: {
     rules: [
       linter,
-      { oneOf: [imgLoad, jsLoad, fontLoad, sassLoad, fileLoad] },
-    ],
+      { oneOf: [imgLoad, jsLoad, fontLoad, sassLoad, fileLoad] }
+    ]
   },
   plugins: [
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(mode),
-      'process.env.PRIVATE': JSON.stringify(isPrivate),
-      'process.env.BROWSER': 'true',
+      "process.env.NODE_ENV": JSON.stringify(mode),
+      "process.env.PRIVATE": JSON.stringify(isPrivate),
+      "process.env.BROWSER": "true"
     }),
     new HtmlWebpackPlugin({
-      template: path.resolve('src', 'public', 'index.html'),
-      minify: true,
+      template: path.resolve("src", "public", "index.html"),
+      minify: true
     }),
     new MiniCssExtractPlugin({
-      filename: 'static/styles/main.css',
+      filename: "static/styles/main.css"
     }),
-    new webpack.HotModuleReplacementPlugin(),
+    new webpack.HotModuleReplacementPlugin()
   ],
-  devtool: 'inline-source-map',
+  devtool: "inline-source-map",
   devServer: {
     hot: true,
-    host: '0.0.0.0',
+    host: "0.0.0.0",
     port: port,
     quiet: true,
-    clientLogLevel: 'silent',
+    clientLogLevel: "silent",
     compress: true,
-    contentBase: path.resolve(__dirname, '..', 'src', 'public'),
+    contentBase: path.resolve(__dirname, "..", "src", "public"),
     historyApiFallback: true,
     before: () => {
       console.log(
-        colors.bold.white(
-          `Server currently listening :\n`
-        ) +
-        colors.reset.yellow(
-          `\nMachine access: http://localhost:${port}\n`
-        ) +
-        colors.reset.yellow(
-          `Network access: http://${ip()}:${port}`
-        )
+        colors.bold.white(`Server currently listening :\n`) +
+          colors.reset.yellow(
+            `\nMachine access: http://localhost:${port}\n`
+          ) +
+          colors.reset.yellow(`Network access: http://${ip()}:${port}`)
       );
 
       if (!isLinux && !isWindows) {
@@ -150,10 +145,10 @@ module.exports = {
           )}"`,
           {
             cwd: path.resolve(__dirname),
-            stdio: 'ignore',
+            stdio: "ignore"
           }
         );
       }
-    },
-  },
+    }
+  }
 };
--- config/webpack-production.js
+++ config/webpack-production.js
@@ -1,54 +1,53 @@
-const path = require('path');
-const process = require('process');
-const webpack = require('webpack');
-const autoprefixer = require('autoprefixer');
-const Critters = require('critters-webpack-plugin');
-const nodeExternals = require('webpack-node-externals');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CompressionPlugin = require('compression-webpack-plugin');
+const path = require("path");
+const process = require("process");
+const webpack = require("webpack");
+const autoprefixer = require("autoprefixer");
+const Critters = require("critters-webpack-plugin");
+const nodeExternals = require("webpack-node-externals");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CompressionPlugin = require("compression-webpack-plugin");
 const OptimizeCSS = require("optimize-css-assets-webpack-plugin");
-const eslintFormatter = require('react-dev-utils/eslintFormatter');
+const eslintFormatter = require("react-dev-utils/eslintFormatter");
+const TerserPlugin = require("terser-webpack-plugin");
 
 const isPrivate = process.env.PRIVATE || false;
 
-if (!process.env.NODE_ENV) process.env.NODE_ENV = 'production';
+if (!process.env.NODE_ENV) process.env.NODE_ENV = "production";
 const mode = process.env.NODE_ENV;
 
 const linter = {
   test: /\.(js|jsx|mjs)$/,
-  enforce: 'pre',
+  enforce: "pre",
   use: [
     {
       options: {
         formatter: eslintFormatter,
-        eslintPath: require.resolve('eslint'),
-        
+        eslintPath: require.resolve("eslint")
       },
-      loader: require.resolve('eslint-loader'),
-    },
+      loader: require.resolve("eslint-loader")
+    }
   ],
-  include: path.resolve(__dirname, 'src'),
+  include: path.resolve(__dirname, "src")
 };
 
 const imgLoad = {
   test: /\.(gif|png|jpe?g)$/i,
-  loader: require.resolve('url-loader'),
+  loader: require.resolve("url-loader"),
   options: {
     limit: 10000,
-    name: 'static/media/[name].[hash:8].[ext]',
-  },
+    name: "static/media/[name].[hash:8].[ext]"
+  }
 };
 
 const jsLoad = {
   test: /\.(js|jsx|mjs)$/,
-  loader: require.resolve('babel-loader'),
+  loader: require.resolve("babel-loader"),
   options: {
-    cacheDirectory: true,
+    cacheDirectory: true
   },
-  exclude: /(node_modules)/,
+  exclude: /(node_modules)/
 };
 
 const sassLoad = {
@@ -56,123 +55,48 @@ const sassLoad = {
   use: [
     MiniCssExtractPlugin.loader,
     {
-      loader: require.resolve('css-loader'),
+      loader: require.resolve("css-loader")
     },
     {
-      loader: require.resolve('postcss-loader'),
+      loader: require.resolve("postcss-loader"),
       options: {
         autoprefixer: {
-          browsers: ["last 2 versions"],
+          browsers: ["last 2 versions"]
         },
-        plugins: () => [
-          autoprefixer,
-        ],
-      },
+        plugins: () => [autoprefixer]
+      }
     },
     {
-      loader: require.resolve('sass-loader'),
-      options: {},
-    },
-  ],
+      loader: require.resolve("sass-loader"),
+      options: {}
+    }
+  ]
 };
 
 const fileLoad = {
   exclude: [/\.(js|jsx|mjs)$/, /\.html$/, /\.json$/],
-  loader: require.resolve('file-loader'),
+  loader: require.resolve("file-loader"),
   options: {
-    name: 'static/media/[name].[hash:8].[ext]',
-  },
+    name: "static/media/[name].[hash:8].[ext]"
+  }
 };
 
 const browserOptimization = {
+  minimize: true,
   minimizer: [
-    new UglifyJsPlugin({
-      'cache': true,
-      'parallel': true,
-      'uglifyOptions': {
-        'sourceMap': true,
-        'output': { comments: false },
-        'mangle': true,
-        'warnings': false,
-        'compress': {
-          'properties': true,
-          'keep_fargs': false,
-          'pure_getters': true,
-          'collapse_vars': true,
-          'sequences': true,
-          'dead_code': true,
-          'drop_debugger': true,
-          'comparisons': true,
-          'conditionals': true,
-          'evaluate': true,
-          'booleans': true,
-          'loops': true,
-          'passes': 4,
-          'unused': true,
-          'hoist_funs': true,
-          'if_return': false,
-          'join_vars': false,
-          'reduce_vars': false,
-          'drop_console': true,
-          'pure_funcs': [
-            'classCallCheck',
-            '_classCallCheck',
-            '_possibleConstructorReturn',
-            'Object.freeze',
-            'invariant',
-            'warning',
-          ],
-        },
-      },
-      sourceMap: true,
-    }),
-    new OptimizeCSS({}),
-  ],
+    new TerserPlugin({
+      parallel: true
+    })
+  ]
 };
 
 const serverOptimization = {
+  minimize: true,
   minimizer: [
-    new UglifyJsPlugin({
-      'cache': true,
-      'parallel': true,
-      'uglifyOptions': {
-        'sourceMap': true,
-        'output': { comments: false },
-        'mangle': true,
-        'warnings': false,
-        'compress': {
-          'properties': true,
-          'keep_fargs': false,
-          'pure_getters': true,
-          'collapse_vars': true,
-          'sequences': true,
-          'dead_code': true,
-          'drop_debugger': true,
-          'comparisons': true,
-          'conditionals': true,
-          'evaluate': true,
-          'booleans': true,
-          'loops': true,
-          'passes': 4,
-          'unused': true,
-          'hoist_funs': true,
-          'if_return': false,
-          'join_vars': false,
-          'reduce_vars': false,
-          'drop_console': false,
-          'pure_funcs': [
-            'classCallCheck',
-            '_classCallCheck',
-            '_possibleConstructorReturn',
-            'Object.freeze',
-            'invariant',
-            'warning',
-          ],
-        },
-      },
-      sourceMap: true,
-    }),
-  ],
+    new TerserPlugin({
+      parallel: true
+    })
+  ]
 };
 
 const aliases = {
@@ -182,100 +106,94 @@ const aliases = {
 
 const browserConfig = {
   mode: mode,
-  stats: 'errors-only',
-  entry: './src/browser/index.js',
-  target: 'web',
-  name: 'browser',
+  stats: "errors-only",
+  entry: "./src/browser/index.js",
+  target: "web",
+  name: "browser",
   optimization: browserOptimization,
   resolve: {
-    alias: aliases,
+    alias: aliases
   },
   output: {
-    path: path.resolve(__dirname, '..', 'build'),
-    filename: 'static/scripts/bundle.[hash:8].js',
-    publicPath: '/',
+    path: path.resolve(__dirname, "..", "build"),
+    filename: "static/scripts/bundle.[hash:8].js",
+    publicPath: "/"
   },
   module: {
-    rules: [
-      linter,
-      { oneOf: [imgLoad, jsLoad, sassLoad, fileLoad] },
-    ],
+    rules: [linter, { oneOf: [imgLoad, jsLoad, sassLoad, fileLoad] }]
   },
   plugins: [
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(mode),
-      'process.env.PRIVATE': JSON.stringify(isPrivate),
-      'process.env.BROWSER': 'true',
+      "process.env.NODE_ENV": JSON.stringify(mode),
+      "process.env.PRIVATE": JSON.stringify(isPrivate),
+      "process.env.BROWSER": "true"
     }),
     new CopyWebpackPlugin([
       {
-        from: path.resolve(__dirname, '..', 'src', 'public', '**/*'),
-        to: path.resolve(__dirname, '..', 'build'),
-        ignore: ['index.html'],
-        flatten: true,
-      },
+        from: path.resolve(__dirname, "..", "src", "public", "**/*"),
+        to: path.resolve(__dirname, "..", "build"),
+        ignore: ["index.html"],
+        flatten: true
+      }
     ]),
     new HtmlWebpackPlugin({
-      template: path.resolve('src', 'public', 'index.html'),
-      filename: path.resolve('build', 'templates', 'index.html'),
-      minify: true,
+      template: path.resolve("src", "public", "index.html"),
+      filename: path.resolve("build", "templates", "index.html"),
+      minify: true
     }),
     new MiniCssExtractPlugin({
-      filename: 'static/styles/main.[hash:8].css',
+      filename: "static/styles/main.[hash:8].css"
     }),
     new Critters({
-      preload: 'js-lazy',
-      logLevel: 'silent',
+      preload: "js-lazy",
+      logLevel: "silent",
       pruneSource: false,
-      minify: true,
+      minify: true
     }),
     new CompressionPlugin({
       minRatio: 1,
       exclude: /^(favicon|apple|android|safari|mstile).*/,
       test: /\.(jpe?g|png|gif|js|css|svg)$/,
-      deleteOriginalAssets: true,
-    }),
-  ].filter(Boolean),
+      deleteOriginalAssets: true
+    })
+  ].filter(Boolean)
 };
 
 const serverConfig = {
   mode: mode,
-  stats: 'errors-only',
-  entry: './src/server/index.js',
-  target: 'node',
-  name: 'server',
+  stats: "errors-only",
+  entry: "./src/server/index.js",
+  target: "node",
+  name: "server",
   optimization: serverOptimization,
   resolve: {
-    alias: aliases,
+    alias: aliases
   },
   externals: [nodeExternals()],
   output: {
-    path: path.resolve(__dirname, '..', 'build'),
-    filename: 'server.js',
-    publicPath: '/',
+    path: path.resolve(__dirname, "..", "build"),
+    filename: "server.js",
+    publicPath: "/"
   },
   module: {
-    rules: [
-      linter,
-      { oneOf: [imgLoad, jsLoad, sassLoad, fileLoad] },
-    ],
+    rules: [linter, { oneOf: [imgLoad, jsLoad, sassLoad, fileLoad] }]
   },
   plugins: [
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(mode),
-      'process.env.PRIVATE': JSON.stringify(isPrivate),
-      'process.env.BROWSER': 'false',
+      "process.env.NODE_ENV": JSON.stringify(mode),
+      "process.env.PRIVATE": JSON.stringify(isPrivate),
+      "process.env.BROWSER": "false"
     }),
     new MiniCssExtractPlugin({
-      filename: 'static/styles/main.[hash:8].css',
+      filename: "static/styles/main.[hash:8].css"
     }),
     new CompressionPlugin({
       minRatio: 1,
       exclude: /^(favicon|apple|android|safari|mstile).*/,
       test: /\.(jpe?g|png|gif|css|svg)$/,
-      deleteOriginalAssets: true,
-    }),
-  ].filter(Boolean),
+      deleteOriginalAssets: true
+    })
+  ].filter(Boolean)
 };
 
 module.exports = [browserConfig, serverConfig];
